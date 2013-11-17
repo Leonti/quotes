562c84acd5afc8fac62bdcb384d741499f6fff30
Leonty Belskiy <prishelec@gmail.com>
2013-11-17 18:26:51 +0100

    Improve embedding page

--- src/main/ui/app/scripts/controllers/QuoteForm.js
+++ src/main/ui/app/scripts/controllers/QuoteForm.js
@@ -6,7 +6,7 @@ angular.module('uiApp')
 	  $scope.addQuote = function() {
 		  		  
 		  var quote = angular.copy($scope.quote);
-		  quote.tags = $('input[name=hidden-tags]').val().split(',');
+		  quote.tags = $('input[name=tags]').val().split(',');
 		  quote.when = $scope.quote.when.getTime() / 1000;
 		  
 		  quoteService.create(quote).then(function(createdQuote) {
